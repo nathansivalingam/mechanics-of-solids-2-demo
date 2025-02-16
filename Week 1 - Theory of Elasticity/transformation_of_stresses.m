9c25e76515dae3e16ca585650dbe6ced9d0ee357
nathansivalingam <nathansivalingampersonal@gmail.com>
2025-02-16 22:25:02 +1100

    week 1, problem 1

---
+++ Week 1 - Theory of Elasticity/transformation_of_stresses.m
@@ -0,0 +1,16 @@
+clc
+% Problem 1 (in MPA)
+sigma_xx = 80; % is +ve because it is facing outwards
+sigma_yy = 80; % is +ve because it is facing outwards
+tao_xy = 45; % is +ve because it is facing top-left and bottom-right
+theta_p = -60; % -ve because the angle is clockwise
+
+% Stress transformation
+sigma_xdash = 0.5 * (sigma_xx + sigma_yy) + 0.5 * (sigma_xx - sigma_yy) * cosd(2 * theta_p) + tao_xy * sind(2 * theta_p);
+fprintf("sigma_xdash %.4f MPA\n", sigma_xdash)
+
+sigma_ydash = 0.5 * (sigma_xx + sigma_yy) - 0.5 * (sigma_xx - sigma_yy) * cosd(2 * theta_p) - tao_xy * sind(2 * theta_p);
+fprintf("sigma_ydash = %.4f MPA\n", sigma_ydash)
+
+tao_xdashydash = (-((sigma_xx - sigma_yy)) / 2) * sind(2 * theta_p) + tao_xy * cosd(2 * theta_p);
+fprintf("tao_xdashydash = %.4f MPA", tao_xdashydash)
